50c12170d14673cb94f652ccab110738b3efcafe
Qmun14 <mamunramdhan@gmail.com>
2023-04-26 08:10:02 +0700

    Nested Routes | Mengadopsi schema atomic design dimana dalam suatu file folder, index.tsx lah yg menjadi default root sebuah page nya

---
+++ pages/blog/category.tsx
@@ -0,0 +1,7 @@
+export default function category() {
+  return (
+    <div>
+      <h2>This is category blog page</h2>
+    </div>
+  )
+}
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+export default function index() {
+  return (
+    <div>
+      <h2>This is blog page</h2>
+    </div>
+  )
+}
