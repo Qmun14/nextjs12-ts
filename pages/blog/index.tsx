44c96f40961127023c52de060271677dc1623816
Qmun14 <mamunramdhan@gmail.com>
2023-04-26 09:32:32 +0700

    Query Parameters | Mengekstrak/menangkap query parameter pada url

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,11 @@
+import { useRouter } from 'next/router';
+
 export default function index() {
+  const router = useRouter();
+  const { page, limit } = router.query;                   //? url ==>   http://localhost:3000/blog?page=5&limit=18
   return (
     <div>
-      <h2>This is blog page</h2>
-    </div>
+      <h2>This is blog page: {page} limit: {limit} </h2>
+    </div>                                                //! outout => This is blog page: 5 limit: 18
   )
 }
